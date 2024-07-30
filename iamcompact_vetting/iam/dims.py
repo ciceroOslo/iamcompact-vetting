4de803482522dd96e9f7ff0f20a195a8d5020269
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2024-07-31 00:00:46 +0200

    Changed IamDimNames from enum to dataclass

--- iamcompact_vetting/iam/dims.py
+++ iamcompact_vetting/iam/dims.py
@@ -1,21 +1,22 @@
 """Module for definitin dimension (column) names for IAM datasets."""
-import enum
+from typing import NewType
+from dataclasses import dataclass
 
 
+class IamDimName(str):
+    """String type used for IAM dimension names."""
+    ...
 
-class IamDim(str, enum.Enum):
+@dataclass(kw_only=True, frozen=True)
+class IamDimNames:
     """Dimension (column) names used by `pyam.IamDataFrame` objects."""
-    MODEL = 'model'
-    SCENARIO = 'scenario'
-    REGION = 'region'
-    VARIABLE = 'variable'
-    UNIT = 'unit'
-    TIME = 'year'
-
-    def __str__(self) -> str:
-        return str(self.value)
-
-    def __repr__(self) -> str:
-        return repr(self.value)
-
+    MODEL: IamDimName = IamDimName('model')
+    SCENARIO: IamDimName = IamDimName('scenario')
+    REGION: IamDimName = IamDimName('region')
+    VARIABLE: IamDimName = IamDimName('variable')
+    UNIT: IamDimName = IamDimName('unit')
+    TIME: IamDimName = IamDimName('year')
 ###END class IamDim
+
+DIM: IamDimNames = IamDimNames()
+"""`IamDimNames` instance with default dimension names IamDataFrames."""
