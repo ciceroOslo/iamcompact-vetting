28026ff049e506eb9a985d92044bb66cc38b5749
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2024-07-31 16:35:50 +0200

    Initial attempt at region/time aggregation specs for TimeseriesRefCriterion
    Will need to be modified further.

--- iamcompact_vetting/iam/timeseries_criteria_core.py
+++ iamcompact_vetting/iam/timeseries_criteria_core.py
@@ -9,11 +9,13 @@ methods of the `pathways-ensemble-analysis` package (`pea`).
 """
 import typing as tp
 from collections.abc import Iterable, Callable
+import dataclasses
 import functools
 import logging
 
 import pyam
 import pandas as pd
+from pandas.core.groupby import SeriesGroupBy
 import pathways_ensemble_analysis as pea
 from pathways_ensemble_analysis.criteria.base import Criterion
 
@@ -22,24 +24,117 @@ from iamcompact_vetting.iam.dims import IamDimName, DIM
 
 
 
+@dataclasses.dataclass(frozen=True)
+class AggFuncTuple:
+    """Class to hold an aggregation function and its parameter values.
+    
+    Fields
+    ----------
+    func : Callable[[pandas.Series], pandas.Series] or str
+        The aggregation function to be applied, or the name of a method of
+        `pandas.SeriesGroupBy` to be called.
+    args : Iterable, optional
+        The positional arguments to be passed to the aggregation function.
+    kwargs : dict, optional
+        The keyword arguments to be passed to the aggregation function.
+    """
+    class AggFunc(tp.Protocol):
+        def __call__(self, s: pd.Series, *args, **kwargs) -> pd.Series:
+            ...
+    ###END class AggFuncTuple.AggFunc
+
+    func: AggFunc | str
+    args: Iterable[tp.Any] = ()
+    kwargs: dict[str, tp.Any] = dataclasses.field(default_factory=dict)
+
+    def __post_init__(self):
+        if isinstance(self.func, str):
+            # Check that the attribute named `func` of `pandas.SeriesGroupBy`
+            # is a method of `pandas.SeriesGroupBy`.
+            if not callable(getattr(SeriesGroupBy, self.func)):
+                raise AttributeError(
+                    f'`{self.func}` is not a method of `pandas.SeriesGroupBy`.'
+                )
+            object.__setattr__(self, 'func',
+                               getattr(pd.Series.groupby, self.func))
+        if not callable(self.func):
+            raise TypeError('`func` must be a string or callable.')
+    ###END def AggFuncTuple.__post_init__
+
+    def get_func(self) -> Callable[[pd.Series], pd.Series]:
+        """"Get an aggregation function to be applied to a pandas.Series.
+        
+        This method will return a partial function with the given positional
+        and keyword arguments applied to it. If `self.func` is a string, it
+        will first be looked up in the `pandas.SeriesGroupBy` methods.
+
+        Returns
+        -------
+        Callable[[pandas.Series], pandas.Series]
+            The aggregation function to be applied to the given
+            `pandas.Series`.
+        """
+        assert callable(self.func)
+        return functools.partial(
+            self.func,
+            *self.args,
+            **self.kwargs
+        )
+    ###END def AggFuncTuple.get_func
+
+###END class AggFuncTuple
+
+
 class TimeseriesRefCriterion(Criterion):
     """Base class for criteria that compare IAM output timeseries.
 
     This class is a subclass of `pea.Criterion` that is designed to compare
     year-by-year differences between a given IAM output timeseries and a
-    reference timeseries. Unlike most `pea.Criterion` subclasses and methods,
+    reference timeseries. In addition to the method `get_value` that all
+    `pea.Criterion` subclasses use to provide a single value for a given
+    pathway, this class is designed to permit comparisons for all years and for
+    multiple regions simultaneously, and therefore provides a method
+    `compare` that can be used to get a fully disaggregated comparison (for each
+    year and available region) or a partially aggregated one (aggregated over
+    only time or only regions).
+
+    The `get_value` method must nevertheless return a single value for a given
+    pathway, i.e., a `pandas.Series` with only the levels `model` and
+    `scenario` in the index. To achieve this, the user must pass functions that
+    aggregate over regions and over time through the `region_agg` and `time_agg`
+    parameters of the `__init__` method.
+
+    Unlike most `pea.Criterion` subclasses and methods,
     which return a single value for an entire model/scenario or a Series with a
     single value for each model/scenario, the methods of this class return a
     Series with a value for each year for a single model/scenario pair, or a
     Series with an additional index level for the year and values for each year
     for each model/scenario pair.
 
+    Note that unlike the `pathways-ensemble-analysis.Criterion` base class,
+    this class is intended to be able to check data for multiple regions at
+    once, and the `__init__` method therefore does not take a `region`
+    parameter. Please filter unwanted regions out of both the reference data
+    before passing it to the `__init__` method, and from the data to be vetted
+    before passing it to the `get_values` method. If you only intend to pass in
+    reference data and data to be vetted for a single region, you can pass in
+    `"first"` to the `region_agg` parameter of the `__init__` method to avoid
+    having to construct an aggregation function (the data will then be
+    "aggregated" using the `first` method of the `pandas.GroupBy` class, which
+    simply returns the first value for each group, each of which should only
+    contain a single value if the data only contains a single region).
+
+
     Init parameters
     ---------------
     criterion_name : str
         The name of the criterion.
     reference : pyam.IamDataFrame
-        The reference timeseries to compare against.
+        The reference timeseries to compare against. *NB!* The original passed
+        object itself is stored and compared against, not a copy. This is done
+        to conserve memory, and allow for defining multiple criteria with the
+        same reference without taking up additional memory. Ensure that you do
+        not unintentionally modify the reference object after passing it in.
     comparison_function : callable
         The function to use to compare the timeseries. The function should take
         two `pyam.IamDataFrame` objects as positional arguments and return a
@@ -135,7 +230,7 @@ class TimeseriesRefCriterion(Criterion):
                                           self.broadcast_dims)
         return self.comparison_function(iamdf, ref)
     ###END def TimeseriesRefCriterion.get_values
-    
+
 ###END class TimeseriesRefCriterion
 
 
@@ -165,7 +260,7 @@ def pyam_series_comparison(
         Callable[[pyam.IamDataFrame, pyam.IamDataFrame], pd.Series]
     ]:
     """Convert function comparing `Series` to one comparing `IamDataFrame`s.
-    
+
     The function is designed to be used as a decorator. The decorated function
     must take two `pandas.Series` objects as positional arguments and return a
     `pandas.Series`. By default, the units of the first `Series` will be
