ae162d45e56188afef01d1378bee0ccb136f4e1d
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2024-08-06 13:11:03 +0200

    Defined new variable for solar and wind, and corrected & in variable name.
    In the assessment notebook script, a new variable
    `Secondary Energy|Electricity|Wind and Solar` is now defined, to work with the
    corresponding AR6 vetting criterion. Also, a variable name containing
    `Energy & Industrial Processes` instead of `Energy and Industrial Processes`
    is corrected.

--- notebooks/assess_ar6_vetting_ranges.py
+++ notebooks/assess_ar6_vetting_ranges.py
@@ -76,6 +76,35 @@ iam_df = pyam.concat([other_df, prometheus_CCS_df])
 if len(iam_df.filter(variable='Carbon Capture*').variable) > 0:
     raise RuntimeError('Unexpected `Carbon Capture` variables remaining.')
 
+# %% [markdown]
+# ### Replace `Energy & Industrial Processes` with `Energy and Industrial Processes` in variable names.
+# %%
+iam_df = iam_df.rename(
+    variable={
+        _varname: _varname.replace(
+            "Energy & Industrial Processes", "Energy and Industrial Processes"
+        )
+        for _varname in iam_df.variable
+        if 'Energy & Industrial Processes' in _varname
+    }
+)  # pyright: ignore[reportAssignmentType]
+
+# %% [markdown]
+# ### Define a new variable `Secondary Energy|Electricity|Wind and Solar`
+#
+# Not present in the 1st cycle models, so define it by adding up
+# `Secondary Energy|Electricity|Wind` and `Secondary Energy|Electricity|Solar`.
+iam_df = pyam.concat(
+    [
+        iam_df,
+        iam_df.add(
+            'Secondary Energy|Electricity|Wind',
+            'Secondary Energy|Electricity|Solar',
+            'Secondary Energy|Electricity|Wind and Solar',
+        )
+    ]
+)  # pyright: ignore[reportAssignmentType]
+
 # %%[markdown]
 # # Assess the AR6 vetting ranges
 #
